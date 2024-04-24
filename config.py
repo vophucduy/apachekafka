408dd085a17865473bdc4843171fe50e49b9ceb0
Võ Phúc Duy <vophucduy52@gmail.com>
2024-04-24 15:29:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 config = {
-    "google_api_key": "AIzaSyCIkRgANVcUb9oWYLwvQQITLT0HszNNHjM",
+    "google_api_key": "<your google api>",
     # "youtube_playlist_id": "PL_Hyc1t0lCQEWN8pmeo3XuYfIeGhlsKQo",
-    "youtube_playlist_id": "PLGitS_TmopfQzaD1TOhJfbL5AMPZU2htK",
+    "youtube_playlist_id": "<your youtube playlist>",
 
     # "youtube_playlist_id": "PL2DsUt1fwwFlAjg6ePRBSwUhGvTecIYmv",
 
     "kafka": {
-        "bootstrap.servers": "pkc-12576z.us-west2.gcp.confluent.cloud:9092",
+        "bootstrap.servers": "<your server>",
         "security.protocol": "sasl_ssl",
         "sasl.mechanism": "PLAIN",
-        "sasl.username": "ACAQQMYG6XUQHW4K",
-        "sasl.password": "mwFCTF6IhMUBocD669YzDyVT9eGsQ4Grg4KYSoYZ5Ud7EtYIOfpL0aHayjGYREoQ",
+        "sasl.username": "<your api>",
+        "sasl.password": "<your api secret>",
     },
     "schema_registry": {
-        "url": "https://psrc-817x7r.us-west2.gcp.confluent.cloud",
-        "basic.auth.user.info": "QK6VEXVYH3WGEVG4:G2aUBIPUf3gbhB2at/h2HNvEp0zc8osSgWOzqqvKohZZHc9dBDVfSQ0JyGHj6zL7",
+        "url": "<your endpoint>",
+        "basic.auth.user.info": "<>:G2aUBIPUf3gbhB2at/h2HNvEp0zc8osSgWOzqqvKohZZHc9dBDVfSQ0JyGHj6zL7",
     }
 }
